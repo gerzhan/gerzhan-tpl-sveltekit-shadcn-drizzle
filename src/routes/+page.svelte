52262f4509f09f8f801b3476aa75bb8d80840b68
Gerzhan <nikolay.gerzhan@gmail.com>
2024-01-05 08:34:34 +0700

    chore: add public env variables

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
-<h1 class="bg-green-400">Welcome to PaySubscription</h1>
+<script lang="ts">
+  import { PUBLIC_SYSTEM_NAME, PUBLIC_DOMAIN_NAME } from '$env/static/public';
+  const systemName = PUBLIC_SYSTEM_NAME;
+  const domainName = PUBLIC_DOMAIN_NAME;
+</script>
+
+<h1 class="bg-green-400">Welcome to {systemName}</h1>
 <p>
-  Visit <a
-    class="text-red-600"
-    href="https://paysubscription.ru">paysubscription.ru</a> to read the documentation
+  Visit <a class="text-red-600" href="https://{domainName}">{domainName}</a> to read the documentation
 </p>
